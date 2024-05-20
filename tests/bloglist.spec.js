b61326d847e67c94c1d7641c7aa9b6936cce5a99
franss <franss40@gmail.com>
2024-05-20 22:00:26 +0200

    exercises 5.18, part 2

--- tests/bloglist.spec.js
+++ tests/bloglist.spec.js
@@ -1,7 +1,15 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test")
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: 'Hellas',
+        username: 'Hellas',
+        password: 'hellas'
+      }
+    })
     await page.goto("http://localhost:5173")
   })
 
@@ -15,5 +23,26 @@ describe("Blog app", () => {
     await expect(
       await page.getByRole("button", { name: 'login' })
     ).toBeVisible()
+  })
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await page.getByRole("textbox", { name: "username" }).fill("Hellas")
+      await page.getByRole("textbox", { name: "password" }).fill("hellas")
+      await page.getByRole("button", { name: "login" }).click()
+
+      await expect(await page.getByText("Create Blog")).toBeDefined()
+      await expect(await page.getByText("blogs")).toBeDefined()
+
+      await expect(await page.getByRole("button", { name: "logout" })).toBeDefined()
+    })
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await page.getByRole('textbox', { name: 'username' }).fill('Hellas')
+      await page.getByRole("textbox", { name: "password" }).fill("eeeellas")
+      await page.getByRole("button", { name: "login" }).click()
+      
+      await expect(await page.getByText("Wrong credentials")).toBeDefined()
+    })
   })
 })
